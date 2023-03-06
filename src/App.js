940ad5ffb654d63d21d3998c6cc0b95ac9c233c2
Raisa Rafi <rafi.raisaar@gmail.com>
2023-03-06 16:44:40 -0700

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 function App() {
-  return <h1>Lotion</h1>;
+  return (
+    <nav>
+      <span class="icon" onclick="toggleSidebar()">&#9776;</span>
+      <div class="stacked">
+        <h1 class="title">
+          Lotion
+        </h1>
+        <div class="subtitle">
+          <strong>Like Notion, but worse.</strong>
+        </div>
+      </div>
+    </nav>
+  );
+
+  // function toggleSidebar() {
+  //   var containerDiv = document.getElementById("right-column");
+  //   console.log(containerDiv.classList);
+  //   containerDiv.classList.toggle("invisible");
+  // }
 }
 
 export default App;
