6d3c1688daef7358c1d34bd9151a8445c5e0b724
Raisa Rafi <rafi.raisaar@gmail.com>
2023-03-08 21:33:16 -0700

    search-bar not working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+
+
 
 function App() {
 
+
   const [note, setNewNote] = useState([]);
+  const [showEditor, setShowEditor] = useState(false);
+  const [showDiv, setShowDiv] = useState([]);
+  const[title, setTitle] = useState("Untitled");
+  const[content, setContent] = useState("");
+
+
+  function createSidebarNote() {
+    const newNote = [...note];
+    newNote.push(<div class="new-note">Untitled</div>);
+    setNewNote(newNote);
+  }
+ 
+  function createDiv() {
+    const newDiv = [showDiv];
+    newDiv.push(<div><input type="text" value={title} onChange={handleTitleChange}></input></div>)
+    newDiv.push(<input type="datetime-local"/>)
+    newDiv.push(<button onClick={handleSave}>Save</button>)
+    newDiv.push(<button>Edit</button>)
+    setShowDiv(newDiv);
+  }
+
+
+  function createEditor() {
+    setShowEditor(true);
+  }
+
+
+  function handleNoteChange(event) {
+    setShowDiv(event.target.value);
+  }
 
 
   function toggleSidebar() {
@@ -11,46 +48,85 @@ function App() {
     containerDiv.classList.toggle("invisible");
   }
 
-  function createNewNote() {
-    const newNote = [...note];
-    newNote.push(<div class="new-note" key={new Date().getTime()}>Untitled</div>);
-    setNewNote(newNote);    
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleContentChange = (value) => {
+    setContent(value);
   }
+
+
+  const handleSave = () => {
+    // Save the note to localStorage
+    const note = { title, content };
+    localStorage.setItem(Date.now(), JSON.stringify(note));
+  };
+
+
+
+
+  <>
+  </>
+
 
   return (
     <>
+    {/* <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<About />}></Route>
+        <Route path="/skills" element={<Skills />}></Route>
+      </Routes>
+    </BrowserRouter> */}
       <nav>
-        <span id="menu-icon" onClick={toggleSidebar}>&#9776;</span>
-        <div class="stacked">
-          <h1 class="title">
-            Lotion
-          </h1>
-          <div class="subtitle">
-            <strong>Like Notion, but worse.</strong>
-          </div>
+        <div class="title">
+          Lotion
         </div>
+        <div class="subtitle">
+          Like Notion, but worse.
+        </div>
+        <span id="menu-icon" onClick={toggleSidebar}>&#9776;</span>
       </nav>
+
+
+
 
       <div id="container" class="invisible">
         <div id="left-column">
           <div>
-            <div id="notes">Notes
-              <span>
-                <button onClick={createNewNote}>+</button>
-              </span>
+            <div class="notes-plus">
+              <span id="notes">Notes</span>
+              <span id="plus-icon" onClick={() => {
+                createSidebarNote();
+                createDiv();
+                createEditor();
+              }}>+</span>
             </div>
             <p>{note}</p>
           </div>
         </div>
-        
+       
         <div id="right-column">
-          <div id="create-note-message">Select a note, or create a new one</div>
+          <div>{showDiv}</div>
+          {/* <input type="text" value={title} onChange={handleTitleChange} /> */}
+          {/* <div id="untitled-top">{showDiv}</div> */}
+          {showEditor ? (
+            <>
+              <ReactQuill
+                className="quill"
+                theme="snow"
+                onChange={handleNoteChange}
+              />
+            </>
+          ) : (
+            <div id="create-note-message">Select a note, or create a new one</div>
+          )}
         </div>
       </div>
-
     </>
   );
-  
 }
 
-export default App
+
+export default App;
