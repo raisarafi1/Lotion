b086aafa6e67bd800b39dbc25b9acfeda665fc19
Raisa Rafi <rafi.raisaar@gmail.com>
2023-03-09 23:53:09 -0700

    final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-
-
 
 
 function App() {
@@ -12,33 +9,56 @@ function App() {
   const [note, setNewNote] = useState([]);
   const [showEditor, setShowEditor] = useState(false);
   const [showDiv, setShowDiv] = useState([]);
-  const[title, setTitle] = useState("Untitled");
-  const[content, setContent] = useState("");
+  const[items, setItems] = useState([]);
 
 
-  function createSidebarNote() {
+
+
+  function handleAddInput() {
+    setItems([...items, { title: 'Untitled', datetime: new Date(), content: ''}]);
+  }
+
+
+  function handleTitleChange(index, event) {
+    const newItems = [...items];
+    newItems[index] = { ...newItems[index], title: event.target.value };
+    setItems(newItems);
+    localStorage.setItem(`title-${index}`, event.target.value);
+  }
+
+
+  function handleDatetimeChange(index, event) {
+    const newItems = [...items];
+    newItems[index] = { ...newItems[index], datetime: new Date(event.target.value) };
+    setItems(newItems);
+  }
+
+
+  function getLocalDatetime(datetime) {
+    const offset = datetime.getTimezoneOffset() * 60000; // offset in milliseconds
+    const localTime = new Date(datetime.getTime() - offset);
+    return localTime.toISOString().slice(0, 16);
+  }
+
+
+  function handleSaveContent(index) {
+    const newItems = [...items];
+    const content = localStorage.getItem(`content-${index}`);
+    newItems[index] = { ...newItems[index], content };
+    setItems(newItems);
+    setShowEditor(false);
+  }
+
+
+  function createSidebarNote(index) {
     const newNote = [...note];
     newNote.push(<div class="new-note">Untitled</div>);
     setNewNote(newNote);
-  }
- 
-  function createDiv() {
-    const newDiv = [showDiv];
-    newDiv.push(<div><input type="text" value={title} onChange={handleTitleChange}></input></div>)
-    newDiv.push(<input type="datetime-local"/>)
-    newDiv.push(<button onClick={handleSave}>Save</button>)
-    newDiv.push(<button>Edit</button>)
-    setShowDiv(newDiv);
   }
 
 
   function createEditor() {
     setShowEditor(true);
-  }
-
-
-  function handleNoteChange(event) {
-    setShowDiv(event.target.value);
   }
 
 
@@ -49,36 +69,10 @@ function App() {
   }
 
 
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
-  };
-
-  const handleContentChange = (value) => {
-    setContent(value);
-  }
-
-
-  const handleSave = () => {
-    // Save the note to localStorage
-    const note = { title, content };
-    localStorage.setItem(Date.now(), JSON.stringify(note));
-  };
-
-
-
-
-  <>
-  </>
 
 
   return (
     <>
-    {/* <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<About />}></Route>
-        <Route path="/skills" element={<Skills />}></Route>
-      </Routes>
-    </BrowserRouter> */}
       <nav>
         <div class="title">
           Lotion
@@ -90,8 +84,6 @@ function App() {
       </nav>
 
 
-
-
       <div id="container" class="invisible">
         <div id="left-column">
           <div>
@@ -99,26 +91,53 @@ function App() {
               <span id="notes">Notes</span>
               <span id="plus-icon" onClick={() => {
                 createSidebarNote();
-                createDiv();
+                handleAddInput();
                 createEditor();
               }}>+</span>
             </div>
-            <p>{note}</p>
+            <div>{note}</div>
           </div>
         </div>
        
         <div id="right-column">
-          <div>{showDiv}</div>
-          {/* <input type="text" value={title} onChange={handleTitleChange} /> */}
-          {/* <div id="untitled-top">{showDiv}</div> */}
-          {showEditor ? (
-            <>
-              <ReactQuill
-                className="quill"
-                theme="snow"
-                onChange={handleNoteChange}
-              />
-            </>
+          {items.map((item, index) => (
+          <div key={index}>
+            <div>
+            <input
+              type="text"
+              id="untitled-top"
+              value={item.title}
+              onChange={(event) => handleTitleChange(index, event)}
+            />
+            </div>
+            <div>
+              <input
+                type="datetime-local"
+                value={getLocalDatetime(item.datetime)}
+                onChange={(event) => handleDatetimeChange(index, event)}
+              />              
+            </div>
+            <div class="button-positioning"><button class="save-edit" onClick={() => handleSaveContent(index)}>Save</button></div>
+            {item.content && !showEditor ? (
+              <div dangerouslySetInnerHTML={{ __html: item.content }}></div>
+            ) : null}
+          </div>
+        ))}
+
+
+
+
+        {showEditor ? (
+          <ReactQuill
+            className="quill"
+            theme="snow"
+            id="editor-box"
+            defaultValue={showDiv}
+            onChange={(value) => {
+              setShowDiv(value);
+              localStorage.setItem(`content-${items.length - 1}`, value);
+            }}
+          />
           ) : (
             <div id="create-note-message">Select a note, or create a new one</div>
           )}
@@ -129,4 +148,13 @@ function App() {
 }
 
 
+
+
 export default App;
+
+
+
+
+
+
+
